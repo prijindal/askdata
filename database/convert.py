ae8e6b2a5112db44f5590f870e846a0c32063d22
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2017-10-28 17:10:09 +0530

    Modifed convert.py to handle multiple files for same database

--- database/convert.py
+++ database/convert.py
@@ -2,8 +2,8 @@ import csv
 import sys
 import MySQLdb
 
-def readcsv():
-    with open('sample.csv', 'r') as csvfile:
+def readcsv(filename):
+    with open(filename, 'r') as csvfile:
         spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
         rows = list(spamreader)
         return rows
@@ -18,22 +18,30 @@ def connect():
 def disconnect(connection):
     return connection.close()
 
-def attribute_name_parser(attr, created_indexes, replace=True):
+def attribute_name_parser(attr, created_indexes, removed_attrs, replace=True):
+    attr = attr.replace('-',' ')
+    attr = attr.replace('/',' ')
     if len(attr) > 64:
         # [sys.stdout.write(str(x)) for x in attr]
-        caps = filter(lambda x: x.isupper(), list(attr))
-        index = "".join(caps)
+        attr_splited = filter(lambda x: len(x) > 0, attr.split(' '))
+        attr_splited = [i[0].upper() for i in attr_splited]
+        index = "".join(attr_splited)
         if replace:
             if index not in created_indexes:
                 created_indexes[index] = 1
             else:
                 created_indexes[index] += 1
         index = index + str(created_indexes[index])
-        # if replace:
-            # print('{0} => {1}'.format(index, attr))
+        if replace:
+            print('{0} => {1}'.format(index, attr))
         attr = index
     else:
-        attr = attr.replace(" ", "_").lower().replace('-','_').replace('___','_').replace('.','').replace('/_','/')
+        attr = attr.replace(" ", "_").replace('-','_').replace('___','_').replace('.','').replace('/_','/')    
+        attr = attr.lower()
+    for j in removed_attrs:
+        if j in attr:
+            attr = j.join(attr.split(j)[1:])
+    attr = attr.strip('_')
     return attr, created_indexes
 
 def find_attr_type(attr, data):
@@ -58,33 +66,38 @@ def find_attr_type(attr, data):
             return 'TINYINT'
         if minimum >= 0:
             if maximum <= 255:
-                return 'TINYINT UNSIGNED'
-            if maximum <= 65535:
                 return 'SMALLINT UNSIGNED'
-            elif maximum <=  4294967295:
+            if maximum <= 65535:
                 return 'INT UNSIGNED'
+            elif maximum <=  4294967295:
+                return 'BIGINT UNSIGNED'
             elif maximum <= 18446744073709551615:
                 return 'BIGINT UNSIGNED'
         else:
             if maximum <= 127 and minimum >= -128:
-                return 'TINYINT'
-            if maximum <= 32767 and minimum >= -32768:
                 return 'SMALLINT'
-            elif maximum <= 2147483647 and minimum >= - 2147483648:
+            if maximum <= 32767 and minimum >= -32768:
                 return 'INT'
+            elif maximum <= 2147483647 and minimum >= - 2147483648:
+                return 'BIGINT'
             elif maximum <= 9223372036854775807 and minimum >= -9223372036854775808:
                 return 'BIGINT'
         return 'DOUBLE'
 
-def create_table_command(tablename, rows):
+def create_table_command(tablename, rows, additional_attr, removed_attrs):
     tablename = tablename.lower()
+    for i in additional_attr:
+        rows[0].append(i)
+    for i in rows[1:]:
+        for j in additional_attr:
+            i.append(additional_attr[j])
     header = rows[0]
     columns = {}
     created_indexes = {}
     for i in range(len(header)):
         data = [row[i] for row in rows[1:]]
         attr = header[i]
-        attr, created_indexes = attribute_name_parser(attr, created_indexes)      
+        attr, created_indexes = attribute_name_parser(attr, created_indexes, removed_attrs)
         datatype = find_attr_type(attr, data)
         # print(datatype)
         columns[attr] = {
@@ -112,7 +125,7 @@ def create_table_command(tablename, rows):
         for j in range(len(row)):
             val = row[j]
             attr = rows[0][j]
-            attr, created_indexes = attribute_name_parser(attr, created_indexes, False)
+            attr, created_indexes = attribute_name_parser(attr, created_indexes, removed_attrs, False)
             datatype = columns[attr]['type']
             if "VARCHAR" in datatype :
                 val = '"{0}"'.format(val)
@@ -124,12 +137,21 @@ def create_table_command(tablename, rows):
     return command, insert_command
 
 def main():
-    rows = readcsv()
+    rows = readcsv('sample2.csv')
     connection = connect()
-    create_command, insert_command = create_table_command("LANGUAGE", rows)
-    print(create_command)
+    create_command, insert_command = create_table_command("EDUCATION", rows, {'year': '2001'}, ['educational_level'])
+    # print(create_command)
+    # print(insert_command)
     with connection.cursor() as cr:
         cr.execute(create_command)
+        cr.execute(insert_command)
+        connection.commit()
+    
+    rows = readcsv('sample.csv')    
+    create_command, insert_command = create_table_command("EDUCATION", rows, {'year': '2011'}, ['educational_level'])
+    
+    with connection.cursor() as cr:
+        # cr.execute(create_command)
         cr.execute(insert_command)
         connection.commit()
 
